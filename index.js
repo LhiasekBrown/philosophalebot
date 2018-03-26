78200486a1863dff07ebf9c804f99e88f9e94a2e
LhiasekBrown <37776162+LhiasekBrown@users.noreply.github.com>
2018-03-26 15:27:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 const bot = new Discord.Client();
 
 var stupefix
-var prefix = ("*")
+var prefix = (".")
 
 bot.on('ready', function() {
-    bot.user.setGame("Command: *help");
+    bot.user.setGame("Command: .help");
     console.log("I'm Ready !");
 });
        
@@ -17,8 +17,30 @@ bot.on('message', message => {
 
     }
 
-    if (message.content === prefix + "spell" + stupefix + message.mentions.user){
+    if (message.content === prefix + "stupefix"){
         message.reply("Vous venez de stupéfixer");
         console.log("Commade Spell effectuée");
+    }
+
+    if (message.content === prefix + "kick"){
+        let modRole = message.guild.roles.find("name", "Test");
+        if(!message.member.roles.has(modRole.id)) {
+            return message.reply("Tu n'as pas les permissions pour cette commande.").catch(console.error);
+        }
+        if(message.mentions.users.size === 0) {
+            return message.reply("Mentionner l'utilisateur à exclure.").catch(console.error);
+        }
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!kickMember) {
+            return message.reply("Cet utilisateur est introuvable.");
+        }
+        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) {
+            return message.reply("Je n'ai pas la permission KICK_MEMBERS pour faire ceci.").catch(console.error);
+        }
+        kickMember.kick().then(member => {
+            message.reply(`${member.user.username} a été expulsé avec succès.`).catch(consolore.error);
+            message.guild.channels.find("name", "general").send(`**${member.user.username} a été expulsé du Discord par **${message.author.username}**`);
+        }).catch(console.error)
+
     }
 });
